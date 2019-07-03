c16b7d759432ce961a3b48875170ef89bba73a68
Brookee <oliver.brooke.a@gmail.com>
2019-07-03 15:27:34 -0400

    liri spotify

--- liri.js
+++ liri.js
@@ -1,9 +1,82 @@
 require("dotenv").config();
+// Link Key page
+var keys = require("./keys.js");
+//Initialize Spotify
+var SpotifyMus = require("node-spotify-api");
+var spotify = new SpotifyMus(keys.spotify);
+//Axios
+var axios = require("axios");
+// File systems
+var fs = require("fs");
+//Moment
+var moment = require("moment");
 
-var key = require("./keys.js");
+// Capture users input
+var gotype = process.argv[2];
+// console.log(gotype);
+var nodeInput = process.argv.slice(3).join(" ");
+// console.log(nodeInput);
 
-var SpotifyMus = require('node-spotify-api');
-var spotify = new SpotifyMus(key.spotify);
+// Functions
+var apiType = function(gotype, nodeInput) {
+  switch (gotype) {
+    case "concert-this":
+      concert(nodeInput);
+      break;
+    case "spotify-this-song":
+      song(nodeInput);
+      break;
+    case "movie-this":
+      movie(nodeInput);
+      break;
+    case "do-what-it-says":
+      says();
+      break;
+    default:
+      console.log(
+        "Input not recognized. Options: \nconcert-this \ndo-waht-it-says \nspotify-this-song \nmovie-this"
+      );
+  }
+};
 
-var 
+apiType(gotype, nodeInput);
 
+// Bands in town logic **WIP, need to fix undefined**
+function concert() {
+  var artist = nodeInput;
+
+  var concertQuery =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
+  axios.get(concertQuery).then(function(response) {
+    for (var i = 0; i < 10; i++) {
+      console.log("Venue Name: " + response[0].venue.name);
+      console.log("Venue Location: " + response[0].venue.city);
+      console.log("Date of Event: " + response.data[0].datetime);
+    }
+  });
+}
+
+// Spotify Logic, **FIXED song search
+
+function song(nodeInput) {
+  if (nodeInput === undefined) {
+    nodeInput = "I Want it That Way"; // Default Song
+  }
+  spotify.search(
+    {
+      type: "track",
+      query: nodeInput
+    },
+    function(error, data) {
+      if (error) {
+        return console.log("There is an error " + error);
+      }
+      console.log("Artist : " + data.tracks.items[0].artists[0].name);
+      console.log("Song Name: " + data.tracks.items[0].name);
+      console.log("Spotify Preview: " + data.tracks.items[0].href);
+      console.log("Album: " + data.tracks.items[0].album.name);
+    }
+  );
+}
